4f7092ce299129abba4f5965802fc72f12ac8119
Christopher Wood <christopherwood07@gmail.com>
2015-08-17 09:55:33 -0700

    added long options

--- scripts/bl.sh
+++ scripts/bl.sh
@@ -7,21 +7,32 @@ usage() {
 
 # preliminary command line argument check
 if [ "$#" -eq 0 ] || [[ "$1" == "-h" ]]; then
-    echo " HEEEEERE"
     usage
 fi
 
 # spell check and grammar check
 s=0
 g=0
+tops=0
+psfile=""
+topdf=0
+pdffile=""
 
-while getopts "sg" option; do
+while getopts "sg-:" option; do
     case "${option}" in
         s)
             s=1
             ;;
         g)
             g=1
+            ;;
+        ps)
+            tops=1
+            psfile=${OPTARG}
+            ;;
+        pdf)
+            topdf=1
+            pdffile=${OPTARG}
             ;;
         *)
             echo " here" 
