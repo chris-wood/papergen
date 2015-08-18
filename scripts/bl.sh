4d2cb0569540adfced6ca815fd2a601a16ff8a27
Christopher Wood <christopherwood07@gmail.com>
2015-08-18 08:05:44 -0700

    Added bl config options

--- scripts/bl.sh
+++ scripts/bl.sh
@@ -10,16 +10,20 @@ if [ "$#" -eq 0 ] || [[ "$1" == "-h" ]]; then
     usage
 fi
 
-# spell check and grammar check
+# flags and options
 s=0
 g=0
 tops=0
 psfile=""
 topdf=0
 pdffile=""
+texfile=""
 
-while getopts "sg-:" option; do
+while getopts "f:sg-:" option; do
     case "${option}" in
+        f)
+            texfile=${OPTARG}
+            ;;
         s)
             s=1
             ;;
@@ -28,20 +32,33 @@ while getopts "sg-:" option; do
             ;;
         ps)
             tops=1
+            # TODO: check for OPTARG length, and defer to default name
             psfile=${OPTARG}
             ;;
         pdf)
             topdf=1
+            # TODO: same as above
             pdffile=${OPTARG}
             ;;
         *)
-            echo " here" 
             usage
             ;;
     esac
 done
 shift $((OPTIND-1))
 
+echo "Building ${texfile}..."
+
+# TODO: run tex-parser to get raw text
+
+if [ "$s" -eq 1 ]; then
+    echo spell check... 
+    # TODO: run raw text through the spell checker
+fi
+if [ "$g" -eq 1 ]; then
+    echo grammar check...
+    # TODO: run raw text through grammar checker
+fi
 
 echo "s = ${s}"
 echo "g = ${g}"
