6a2c454b1a29dc40901ba2f17432a68e3593c75f
Christopher Wood <christopherwood07@gmail.com>
2015-08-18 08:05:53 -0700

    Tex parser stub

---
+++ scripts/texparser.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+def main(args):
+    # 1. remove comments, packages, etc
+    # 2. ... just get the plain text
+    pass
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
