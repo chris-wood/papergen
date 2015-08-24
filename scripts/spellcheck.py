cf0ca920f459bdefbe9b6411d2f75a926c927bec
Christopher Wood <christopherwood07@gmail.com>
2015-08-24 10:03:01 -0700

    spellcheck

--- scripts/spellcheck.py
+++ scripts/spellcheck.py
@@ -1,36 +1,72 @@
 import os
 import sys
 import enchant
+import string
+from terminal import *
 
 class SpellChecker(object):
     def __init__(self, dictionary):
         self.dictionary = enchant.Dict(dictionary)
 
+    def printSentenceWithError(self, words, index):
+        for wordIndex, word in enumerate(words):
+            if index == wordIndex:
+                cprint("red", word[0])
+            else:
+                cprint("green", word[0])
+
     def checkText(self, text):
-        sentences = text.split(".")
+        sentences = filter(lambda s : len(s) > 0, text.split("\n"))
         for sentence in sentences:
-            words = [(word, index) for index, word in enumerate(sentence.split(" "))]
-            failures = filter(lambda (word, index) : self.dictionary.check(word), words)
-            print "Assessing: " + repr(sentence)
+
+            exclude = set(string.punctuation)
+            words = [(''.join(ch for ch in word.strip() if ch not in exclude), index) for index, word in enumerate(sentence.split(" "))]
+            failures = filter(lambda (word, index) : len(word) > 0 and not self.dictionary.check(word), words)
 
             replacements = []
             for (failure, index) in failures:
-                print failure
-                # 1. print out sentence (minus the word) in green, and the word in red
-                # 2. print out suggestion list
-                # 3. collection suggestions
-                # self.dictionary.suggest(word)
 
+                suggestions = self.dictionary.suggest(failure)
                 replacement = failure
+                if len(suggestions) > 0:
+                    # TODO: need to print the original sentence -- not the reduced verison that has puncuation removed
+                    self.printSentenceWithError(words, index)
+
+                    # print failure, index
+                    # 1. print out sentence (minus the word) in green, and the word in red
+                    # 2. print out suggestion list
+                    # 3. collection suggestions
+
+                    print ""
+                    print "\t0: Skip!"
+                    for changeIndex, change in enumerate(suggestions):
+                        print "\t%d: %s" % (changeIndex + 1, change)
+                    choice = int(raw_input("> "))
+
+                    skipped = False
+                    if (choice == 0):
+                        skipped = True
+
+                    while choice < 0 or choice > (len(suggestions)):
+                        print "Invalid selection. Try again."
+                        choice = int(raw_input("> "))
+                    if not skipped:
+                        replacement = suggestions[int(choice) - 1]
+
+                # TODO: print "replacing <failure> with <selection version>"
                 replacements.append((index, replacement))
+
+                # TODO: replace the word at the index with the replacement
 
 def main(args):
     if len(args) != 1:
         print >> sys.stderr, "usage: python spellchecker.py <filename>"
         exit(1)
     fhandle = open(args[0], "r")
-    # TODO: read the file
+    text = fhandle.read()
+
+    checker = SpellChecker("en_US")
+    checker.checkText(text)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
