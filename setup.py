a53ba228255ec3ec65b3a7a73f5d5b9870186b65
Christopher Wood <christopherwood07@gmail.com>
2016-02-23 08:55:11 -0800

    Moving stuff around

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='papersmith',
+      version='0.1',
+      description='Some thing',
+      url='http://github.com/chris-wood/papersmith',
+      author='COol stuff',
+      author_email='caw@caw.com',
+      license='MIT',
+      packages=['papersmith'],
+      zip_safe=False)
---
+++ tests/papersmith_tests.py
@@ -0,0 +1,18 @@
+from nose.tools import *
+import papersmith
+
+from papersmith import *
+#from papersmith.Reader import *
+
+# def setup():
+#     pass
+#
+# def teardown():
+#     pass
+
+def test_reader():
+    pass
+
+def test_config_parser():
+    # TODO: write some tests for me
+    pass
